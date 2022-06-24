3f414f4ce8d4fe6a911033f1605c9693727d014b
Alchemy5 <104028972+Alchemy5@users.noreply.github.com>
2022-06-23 20:11:44 -0400

    Update manager.py

--- Vision-Manager/manager.py
+++ Vision-Manager/manager.py
@@ -22,27 +22,26 @@ class Manager():
         self.s.bind((host, self.port))
         self.s.listen(5)
         while True:
-            print("No nodes are detected as of yet")
+            print("No nodes are detected as of yet") #waiting for node to connect to manager object
             self.cur_sock, self.client_address = self.s.accept()
             if self.cur_sock != None and self.client_address != None:
                 print("Connection created")
                 break
 
-        #checks whether node is connected
-
+        
+    #communication helper functions
     def deserialize(self,msg):
         msg = json.loads(msg)
-
         arr = np.frombuffer(msg['data'].encode('ISO-8859-1'), dtype=msg['dtype'])
         arr = np.reshape(arr, msg['shape'])
         return arr
+    
     def recv_msg(self, sock):
         # Read message length and unpack it into an integer
         raw_msglen = self.recvall(sock, 4)
         if not raw_msglen:
             return None
         msglen = struct.unpack('>I', raw_msglen)[0]
-
         # Read the message data
         return self.recvall(sock, msglen)
 
@@ -55,7 +54,7 @@ class Manager():
                 return None
             data.extend(packet)
         return data
-
+    #get sample image to see if node camera pipeline is working 
     def takeImage(self):
         if self.cur_sock is None:
             raise Exception("No client currently connnected!")
@@ -80,10 +79,10 @@ class Manager():
                 im = Image.fromarray(data)
                 im = im.save(r"testImage.jpg")
                 print("Image has been saved in your local directory")
-
+    #main inferencing function to run inferencing on the node and recieve outputs from node
     def inference(self):
         #first sends a string to tell the node what is happening
-        # then sends json file containing all instructions
+        #then sends json config file containing all instructions
         msg = "inferencing"
         self.cur_sock.sendall(msg.encode())
         reply = self.cur_sock.recv(1024)
@@ -110,7 +109,7 @@ class Manager():
                         self.end()
                         raise Exception("Inferencing with model has stopped")
                     else:
-                        print(reply)
+                        print(reply) #prints model outputs, imagenet.txt labels
                 elif(custom == True):
                     data_json = self.recv_msg(self.cur_sock)
                     data = json.loads(data_json)
@@ -119,12 +118,12 @@ class Manager():
                         raise Exception("Inferencing with custom model has stopped")
                     else:
                         data = self.deserialize(data_json)
-                        print(data)
+                        print(data) #prints model outputs, tensors
         except:
             self.end()
             raise Exception("Inferencing has stopped")
 
-    def end(self):
+    def end(self): #cleanup function
         msg = "end node"
         self.cur_sock.sendall(msg.encode())
         self.s.close()
